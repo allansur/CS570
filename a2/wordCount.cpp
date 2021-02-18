310987811b8c354671c6ba8fe2061ee2f6498b20
allansur <64333878+allansur@users.noreply.github.com>
2021-02-18 14:41:12 -0800

    command checking function

--- a2/wordCount.cpp
+++ a2/wordCount.cpp
@@ -13,6 +13,21 @@ typedef struct {
 } PROGRESS_STATUS;
 
 int main(int argc, char **argv){
+    string filename;
+    if (argc < 2) {
+        cout << "No file was input, proper usage: ./wordcount 'filename' " << endl;
+        exit(1);
+    }
+    else if (argc == 2){
+        
+        filename = argv[2];
+        if (argv.subtr(argv[2].find_last_of("."),argv[2].length() -1) == ".txt"){
+            filename = argv[2];
+            wordCount(filename);
+        }
+        
+
+    }
     PROGRESS_STATUS ps;
     ps = {0, 0, 0};
     //Do all thread stuff
@@ -27,7 +42,7 @@ int main(int argc, char **argv){
     return 0;
 }
 
-int wordCount(string filename){
+long wordCount(string filename){
     int wc = 0;
     //Open the file here
     //File is opened, words are counted based on whitespace
