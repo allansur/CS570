5832a1d7f5f76b6b7af6efcb483474f9008ef448
allansur <64333878+allansur@users.noreply.github.com>
2021-02-18 13:42:12 -0800

    init

--- a2/wordCount.cpp
+++ a2/wordCount.cpp
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <string>
 
+using namespace std;
+
+int main(int argc, char **argv){
+
+
+
+
+
+
+
+    return 0;
+}
+
+int wordCount(string filename){
+    int wc = 0;
+    //File is opened, words are counted based on whitespace
+    //return word count
+    return wc;
+}
