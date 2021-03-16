b4dc73040fd1f703723bef610c2c9f0ccf10929e
jsongvilay <57165394+jsongvilay@users.noreply.github.com>
2021-03-16 16:57:39 -0700

    added main
    added main

--- a3/map.cpp
+++ a3/map.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 
 MAP * PageLookup(PAGETABLE *PAGETABLE, unsigned int LogicalAddress) {
-    unsigned int = addr = LogicalToPage(LogicalAddress, bitMask[], 2);
+  
     
 }
--- a3/pagetable.cpp
+++ a3/pagetable.cpp
@@ -22,24 +22,3 @@ unsigned int calcBitmask(int start, int length){
     return mask;
 }
 
-//Move Main some other time
-int main(int argc, char **argv) {
-    int idx;
-    int Option;
-    while ( (Option = getopt(argc, argv, "n:o:")) != -1){
-        switch (Option) {
-            case 'n':   //Number of addys to process
-            //optarg will contain the string folliwng -n
-            //Process appropriately (e.g. convert to integer atoi(optarg))
-                break;
-            case 'o': /*produce map of pages */
-            //optarg contains the output method...
-                break;
-            default:
-                //print something about the usage and die...
-                exit(1); //BADFLAG is an error # defined in a header
-        }
-    }
-    idx = optind;
-    return 0;
-}
