a45174f7241ac77dc0e2d130ab89dd293a8eb9a7
jsongvilay <57165394+jsongvilay@users.noreply.github.com>
2021-03-16 18:24:17 -0700

    trace
    trace

--- a3/main.cpp
+++ a3/main.cpp
@@ -10,40 +10,6 @@
 #include <map>
 using namespace std;
 
-void report_logical2offset(uint32_t logical, uint32_t offset) {
-    fprintf(stdout, "%08X -> %08X\n", logical, offset);
-}
-
-void report_summary(unsigned int page_size, unsigned int hits, unsigned int addresses, unsigned int frames_used, unsigned int bytes) {
-    unsigned int misses;
-    double hit_percent;
-
-    printf("Page size: %d bytes\n", page_size);
-    misses = addresses - hits;
-    hit_percent = (double) hits / addresses * 100.0;
-    printf("Addresses processed: %d\n", addresses);
-    printf("Hits: %d (%.2f%%), Misses %d (%.2f%%)\n", hits, hit_percent, misses, 100 - hit_percent);
-    printf("Frames allocated: %d\n", frames_used);
-    printf("Bytes used: %d\n", bytes);
-}
-
-void report_bitmasks(int levels, uint32_t *masks) {
-    printf("Bitmasks\n");
-    for (int i = 0; i < levels; i++){
-        /* show mask entry and move to next */
-        printf("level %d mask %08X\n", i, masks[i]);
-    }
-}
-
-void report_pagemap(uint32_t logical_addr, int levels, uint32_t *pages, uint32_t frame) {
-    printf("%08X: ", logical_addr);
-    for(int i = 0; i < levels; i++) {
-        printf("%X ", pages[i]);
-    }
-    printf("-> %X\n", frame);
-}
-
-
 int main(int argc, char **argv) {
     int idx;
     int Option;
