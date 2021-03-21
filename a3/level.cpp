c9dbcdec639414a6b05c016b2f5f563bc6622ba9
Allan <asur@evolvethefuture.org>
2021-03-20 17:18:23 -0700

    added levels and maap

--- a3/level.cpp
+++ a3/level.cpp
@@ -6,56 +6,102 @@ using namespace std;
 
 //Create a new instance of a level for the page table
 long int tableSize, bitsUsed;
-Level::Level(int d, PAGETABLE *pgTblPtr)
-{
-    Level::depth = d;
-    Level::pageTablePtr = pgTblPtr;
-    Level::nextLevelPtr = nullptr; //idk how to initialize a null pointer
-
-    if (depth < pageTablePtr->numOfLevels)
-    {
-        Level::isLeaf = false;
-        int nextLvlSize = Level::pageTablePtr->entryCount[depth];
-        Level::nextLevelPtr = new Level *[nextLvlSize];
-        for (int i = 0; i < nextLvlSize; i++)
-        {
+Level::Level(int depth, bool isLeafLevel, PAGETABLE *pageTablePtr) {
+    Level::depth = depth;
+    Level::isLeaf = isLeafLevel;
+    Level::pageTablePtr = pageTablePtr;
+    Level::map = nullptr;
+    if (isLeafLevel) {
+        Level::nextLevelPtr = nullptr;
+    } else {
+        int nextLevelSize = Level::pageTablePtr->entryCountArray[depth];
+        Level::nextLevelPtr = new Level *[nextLevelSize];
+        for (int i = 0; i < nextLevelSize; i++) {
             nextLevelPtr[i] = nullptr;
         }
-        tableSize += sizeof(Level) + sizeof(Level *) *Level::pageTablePtr -> entryCount[depth];
-
-        //We set an unsigned int to the next level size to be able to initialize the next level to the correct size of entries from the original page table this level is from
-    }
-    else
-    {
-        isLeaf = true;
-        MAP *map;
-        tableSize += sizeof(Level) + sizeof(map);
     }
 }
-//just changes logical address to page address for virtual paging by ANDing the logical address and our bitmask and then shifting it x amount per level that it's on
-unsigned int LogicalToPage(unsigned int LogicalAddress, unsigned int Mask, unsigned int Shift)
-{
-    return (LogicalAddress & Mask) >> Shift; // We got 1 down!
-}
-//pageinseration method for a level in a page table (void cause not actually returning anything just inserting)
-void Level::pageInsert(unsigned int logicalAddy, unsigned int frameIndex) {
-    if (isLeaf){
-        //store the frame and set the index to valid in the bit map (1) 
-        //If this is already a leaf node, that means we can just set it's flagIndex to 1 to show it's used and set it's frameIndex
-        map -> flagIndex = true;
-        map -> frameIndex = frameIndex;
-        bitsUsed += sizeof(map);
-    }
-    else {
-        //create a new level in the pagetable and set it's depth = curr_depth + one
-        //turn our logical address into a VPA through calling the LTP function.
-        unsigned int pageNum = LogicalToPage(logicalAddy, pageTablePtr -> bitMaskArr[depth], pageTablePtr -> shifters[depth]);
-        //Create a new level in our page table as the next level with the correct depth and pagetable reference to grab entries
-        nextLevelPtr[pageNum] = new Level(depth, pageTablePtr);
-        //save the size of the level created and the pointer
-        bitsUsed += sizeof(Level) + sizeof(Level *);
-    //Insert page into the newly created level.
-        nextLevelPtr[pageNum] -> pageInsert(logicalAddy, frameIndex);
 
+Level::~Level() {
+    if (isLeaf) {
+        delete map;
+    } else {
+        delete[] nextLevelPtr;
     }
+
+}
+
+Level *Level::getSubLevel(int subLevel) {
+    if (isLeaf) {
+        throw runtime_error("Unable to get Sub-Level from a Leaf Level");
+    }
+    if (nextLevelPtr[subLevel] == nullptr) {
+        bool isChildLeaf = pageTablePtr->levelCount == depth + 1;
+        nextLevelPtr[subLevel] = new Level(depth + 1, isChildLeaf, pageTablePtr);
+    }
+    return nextLevelPtr[subLevel];
+}
+
+MAP *Level::getMap(int) {
+    if (!isLeaf) {
+        throw runtime_error("Unable to get Map from a non-Leaf Level");
+    }
+    if (mapPtr == nullptr) {
+        int mapSize = Level::pageTablePtr->entryCountArray[depth];
+        mapPtr = new MAP(mapSize);
+    }
+    return mapPtr;
+}
+
+bool Level::insert(unsigned int logicalAddress, unsigned int frameNumber) {
+    unsigned int bitMask = pageTablePtr->levelBitmaskArray[depth];
+    unsigned int shift = pageTablePtr->levelShiftArray[depth];
+    unsigned int location = (logicalAddress & bitMask) >> shift;
+
+    if (isLeaf) {
+        if (map == nullptr) {
+            map = new MAP(pageTablePtr->entryCountArray[depth]);
+        }
+        return map ->insertPageNumber(location, frameNumber);
+    }
+    if (nextLevelPtr[location] == nullptr) {
+        bool isChildLeaf = pageTablePtr->levelCount == depth + 2;
+        nextLevelPtr[location] = new Level(depth + 1, isChildLeaf, pageTablePtr);
+    }
+    return nextLevelPtr[location]->insert(logicalAddress, frameNumber);
+
+}
+
+int Level::getFrameNumber(unsigned int logicalAddress) {
+    unsigned int bitMask = pageTablePtr->levelBitmaskArray[depth];
+    unsigned int shift = pageTablePtr->levelShiftArray[depth];
+    unsigned int location = (logicalAddress & bitMask) >> shift;
+    if (isLeaf) {
+        if (map == nullptr) {
+            return 1
+        } else {
+            return map ->getFrameNumber(location);
+        }
+    } else {
+        if (nextLevelPtr[location] == nullptr) {
+            return 1;
+        } else {
+            return nextLevelPtr[location]->getFrameNumber(logicalAddress);
+        }
+    }
+}
+
+int Level::sizeTotal() {
+    int basicSize = sizeof(depth) + sizeof(isLeaf) + sizeof(pageTablePtr);
+    basicSize += sizeof(nextLevelPtr) + sizeof(map);
+    int subSize = 0;
+    if (isLeaf) {
+        return map == nullptr ? basicSize : basicSize + map->sizeTotal();
+    }
+    for (int i = 0; i < pageTablePtr->entryCountArray[depth]; i++) {
+        if (nextLevelPtr[i] != nullptr) {
+            subSize += nextLevelPtr[i]->sizeTotal();
+        }
+    }
+    return basicSize + subSize;
 }
--- a3/map.cpp
+++ a3/map.cpp
@@ -2,6 +2,46 @@
 #include "pagetable.h"
 using namespace std;
 
-MAP *PageLookup(PAGETABLE *PAGETABLE, unsigned int LogicalAddress)
-{
+MAP::MAP(int size) {
+    MAP::size = size;
+    MAP::frameIndex = new int[size];
+    for (int i = 0; i < size; i++) {
+        frameIndex[i] = INVALID;
+    }
+}
+
+MAP::~MAP() {
+    delete[] frameIndex;
+}
+
+int MAP::getFrameNumber(int pageNum) {
+    return frameIndex[pageNum];
+}
+
+bool MAP::insertPageNumber(int pageNum, int frameIndex) {
+    if (frameIndex[pageNum] != -1) {
+        return false;
+    }
+    frameMapping[pageNum] = frameIndex;
+    return true;
+}
+
+int MAP::invalidatePageNumber(int pageNum) {
+    int curr_frame_index = frameMapping[pageNum];
+    frameIndex[pageNum] = -1;
+    return curr_frame_index;
+}
+
+bool MAP::invalidateFrameNumber(int frameNum) {
+    for (int i = 0; i < size; i++) {
+        if (frameIndex[i] == frameNum) {
+            frameIndex[i] = false;
+            return true;
+        }
+    }
+    return false;
+}
+
+int MAP::sizeTotal() {
+    return sizeof(size) + sizeof(frameIndex) + sizeof(int) * size;
 }
