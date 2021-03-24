0d0a2de60fbb8708fe0b3ee808498c66a4593025
jsongvilay <57165394+jsongvilay@users.noreply.github.com>
2021-03-24 16:47:20 -0700

    comments added

--- a3/main.cpp
+++ a3/main.cpp
@@ -16,64 +16,41 @@
 #include <cmath>
 using namespace std;
 
+//main helper function to get the amount of bits we want in a mask
+//i.e if we want the first 3 bytes of an unsigned int we pass this function 0 - 11
+//Kind of our workaround on how to create a bitmask for the offset grabbing specifically
 unsigned createMask(unsigned int first, unsigned int last)
 {
-   unsigned int start = 0;
-   for (unsigned int i = first; i <= last; i++)
-       start |= 1 << i;
+    //starts with unsigned int of 0
+    unsigned int start = 0;
+    //for the first -> last  bits, we shift in a 1 to create a mask
+    for (unsigned int i = first; i < last + 1; i++)
+        start |= 1 << i;
 
-   return start;
+    return start; //return our mask
 }
 
+//start of our driver function
 int main(int argc, char **argv)
 {
-    int idx;
+    //initialization of input options
     int Option;
+    int idx;
     int numberOfAddresses;
     char *outputMode;
-    ifstream tracers;
-
+    //Create an instance of a PAGETABLE pointer pt
     PAGETABLE *pt = new PAGETABLE;
+    //Create instance of our first level pointer
     LEVEL *lv = new LEVEL;
-    idx = optind;
+    //Initialize a vector of all the addresses that are going to be pushed in
     vector<unsigned int> addys;
+    //Set flags for all input parameters to false
     bool nFlag = false, bFlag = false, l2PFlag = false, p2FFlag = false, ofFlag = false, rSum = false;
-    //**************************** FOR TESTING PURPOSES ONLY **************************
-    // int bits = 20;
 
-    
-    // pt->rootPtr = lv;
-    // pt->numOfLevels = levelCount;
-    // cout << "numOfLevels: " << pt->numOfLevels << "\n";
-
-    // pt->bitMaskArr = new unsigned int[levelCount];
-    // pt->shifters = new unsigned int[levelCount];
-    // pt->entryCount = new unsigned int[levelCount];
-    // cout << "bitmaskArr: " << pt->bitMaskArr << "\n";
-    // cout << pt->bitMaskArr[0] << "\n";
-    // cout << "l2a: " << std::hex << LogicalToPage(0x3c654321, 0x0FC00000, 22) << "\n"; // 0x31 aka decimal 49
-
-    // cout << "bitmask: " << calcBitmask(20, 8) << "\n";
-    // cout << "creation: " << createPageTable(pt, 1) << "\n";
-    // cout << "get before"
-    //      << "\n";
-    // cout << pt->frameCount << "\n";
-    // PageInsert(pt, 0x3c654321, pt->frameCount);
-    // PageInsert(pt, 0x3c654322, pt->frameCount);
-    // PageInsert(pt, 0x3c654323, pt->frameCount);
-    // PageInsert(pt, 0x3c654324, pt->frameCount);
-    // PageInsert(pt, 0x3c654325, pt->frameCount);
-    // PageInsert(pt, 0x3c654326, pt->frameCount);
-    // PageInsert(pt, 0x3c654327, pt->frameCount);
-    // cout << pt->frameCount << "\n";
-    // cout << "PageLookup: " << PageLookup(pt, 0x3c654322) << endl;
-    // // cout << map << endl;
-    // cout << pt << "\n";
-    // // testing out functions
-//********************************************************************************
-
-     while ((Option = getopt(argc, argv, "n:o:")) != -1)
+    //While loop to grab our parameters and requirements from the user as given by the professor in pseudocode
+    while ((Option = getopt(argc, argv, "n:o:")) != -1)
     {
+        //Check if either (n || o) || (n && o) is input into the command line
         switch (Option)
         {
         case 'n': //Number of addys to process
@@ -84,34 +61,28 @@ int main(int argc, char **argv)
             break;
         case 'o': /*produce different outputs */
             //optarg contains the output method...
+            //set flags based on what output mode -o "..." is specified by the user
             outputMode = optarg;
             if (strcmp(outputMode, "bitmasks") == 0)
             {
-                // report_bitmasks(levelCount, masks);
                 bFlag = true;
             }
             else if (strcmp(outputMode, "logical2physical") == 0)
             {
-                // report_logical2physical(pt->rootPtr->map->frameIndex, offset);
                 l2PFlag = true;
             }
             else if (strcmp(outputMode, "page2frame") == 0)
             {
-                // report_pagemap(uint32_t logical_addr, int levels,
-                // uint32_t *pages, uint32_t frame);
                 p2FFlag = true;
             }
             else if (strcmp(outputMode, "offset") == 0)
             {
-                // report_logical2offset(uint32_t logical, uint32_t offset);
                 ofFlag = true;
             }
-            else{
-                // report_summary(unsigned int page_size, unsigned int hits,
-                // unsigned int addresses, unsigned int frames_used,
-                // unsigned int bytes);
+            else
+            {
                 rSum = true;
-                }
+            }
             break;
         default:
             //print something about the usage and die...
@@ -120,91 +91,123 @@ int main(int argc, char **argv)
             break;
         }
     }
-    
-    // cout << "bFlag: " << bFlag << endl;
-    string filename = argv[optind];
-    FILE *fp = fopen(filename.c_str(), "r"); 
-    p2AddrTr trace_item;  /* Structure with trace information */
-  bool done = false;
-    if (fp == NULL) { 
-        //Fail to read
-        fprintf(stderr, "Unable to open & read file: %s\n", filename.c_str());
+    //Retrieve the file path from commandline
+    idx = optind;
+    string filename = argv[idx];
+    FILE *fp = fopen(filename.c_str(), "r");
+    //Create p2AddrTr from BYU tracefile to read in addresses
+    p2AddrTr trace_item; /* Structure with trace information */
+    if (fp == NULL)
+    {
+        fprintf(stderr, "Cannot open file: %s\n", filename.c_str());
         exit(1);
-    } 
-  
-    int levelCount = argc - (optind + 1);
-    pt -> numOfLevels = levelCount;
-    int offset = 32 - createPageTable(pt, argv, argc-levelCount);
-    unsigned int addyCount = 0;
-    while (fp) {
-         if (nFlag) {
-             if(numberOfAddresses <= addyCount){
-                 break;
-             }
-             if (NextAddress(fp, &trace_item)){
-                 addys.push_back(trace_item.addr);
-                 unsigned int addy = (unsigned int) trace_item.addr;
-                 if (PageLookup(pt, addy) == NULL) {
-                     pt -> misses++;
-                     PageInsert(pt, addy, pt-> frameCount);
-                     
-                     //OFFSET OUTPUT WORKS NOW
-                    
-                 }
-                 else {
-                 pt -> hits++;
-                }
-                if (p2FFlag){
-                         // ./pagetable -n 20 -o page2frame trace.sample.tr 4 4 12
-                         unsigned int page = addy;
-                         page >>= offset;
-                         MAP *map = PageLookup(pt, addy);
-                         printf("%08X: ", addy);
-                         printf("%X ", page);
-                         printf("-> %X\n", map -> frameIndex);
+    }
 
-                     } 
-                if(ofFlag) {
-                         // ./pagetable -n 20 -o offset trace.sample.tr 4 4 12
-                        //our create mask method basically just shifts a 1 in 0 - levelCount * 4 - 1 amount of times to create a mask to and to get the last x amount of bytes in the address as the offset
-                        unsigned int mask = createMask(0, offset - 1);
-                        unsigned int oHolder = addy & mask;
-                        report_logical2offset(addy, oHolder);
-                    }
-                if (l2PFlag) {
-                    // ./pagetable -n 20 -o logical2physical trace.sample.tr 28
+    //Counter to hold the number of addresses read
+    unsigned int addyCount = 0;
+    //Number of levels will be the last numeric arguments of command line inputs
+    int numOfLevels = argc - (idx + 1);
+    //set our number of levels for the page table to be what was calculated above
+    pt->numOfLevels = numOfLevels;
+    //calculate amount of bits used by calling the createPageTable function and setting it to our previously initialized PAGETABLE pointer pt
+    int bitUsed = createPageTable(pt, argv, argc - numOfLevels);
+    //calculate the offset by doing 32 - bits used returned from the createPageTable fucntion
+    int offset = 32 - bitUsed;
+
+    //Loop while the file is not done
+    while (fp)
+    {
+        //If there is an -n flag detected, we make sure that we do not process more than the given amount
+        if (nFlag)
+        {
+            if (numberOfAddresses <= addyCount)
+            {
+                break;
+            }
+            //Check that there is another address given to us
+            if (NextAddress(fp, &trace_item))
+            {
+                //Append the address into our array addys
+                addys.push_back(trace_item.addr);
+                unsigned int addy = (unsigned int)trace_item.addr;
+                //Check if the page is in the table and if not
+                if (PageLookup(pt, addy) == NULL)
+                {
+                    //increment misses for our pt
+                    pt->misses++;
+                    //insert the page into the pt
+                    PageInsert(pt, addy, pt->frameCount);
+                    //OFFSET OUTPUT WORKS NOW
+                }
+                else
+                {
+                    //the address is in our page table and we can increment hits
+                    pt->hits++;
+                }
+                // if -n page2frame is passed in on command line
+                if (p2FFlag)
+                {
+                    //This method is not fully functional
+                    unsigned int page = addy;
+                    page >>= offset;
+                    MAP *map = PageLookup(pt, addy);
+                    printf("%08X: ", addy);
+                    printf("%X ", page);
+                    printf("-> %X\n", map->frameIndex);
+                }
+                // if -n offset is passed in on command line
+                if (ofFlag)
+                {
+                    // our create mask method basically just shifts a 1 in 0 - numOfLevels * 4 - 1 amount of times to create a mask to and to get the last x amount of bytes in the address as the offset
+                    unsigned int mask = createMask(0, offset - 1);
+                    unsigned int oHolder = addy & mask;
+                    report_logical2offset(addy, oHolder);
+                }
+                // if -n logical2page is passed in on command line
+                if (l2PFlag)
+                {
+                    //create a map pointer that will be pointing the page lookup of our given address
                     MAP *map = PageLookup(pt, addy);
                     int hex = offset / 4;
-                    unsigned int translation = map -> frameIndex * pow(16, hex);
+                    //calculate the amount of bytes we need
+                    unsigned int translation = map->frameIndex * pow(16, hex);
+                    //by calling our fraame number to frameIndex * 16^(hex) wee can insert the fraamea index before our aamount of offset bits
                     unsigned int mask = createMask(0, offset - 1);
+                    //create a mask to grab our offset
                     unsigned int oHolder = (addy & mask) + translation;
+                    //Add our offset to our translation and get correct physical address and pass it through to output function
                     report_logical2physical(addy, oHolder);
-                } 
-             }
-             addyCount++;
-         }
+                }
+            }
+            //increment aaddress count to make sure we have not exceeded given -n number of addresses
+            addyCount++;
+        }
     }
-    if(bFlag) {
-        // ./pagetable -n 10000 -o bitmasks trace.sample.tr 4 4 12
+    // if -n bitmasks is passed in on command line
+    if (bFlag)
+    {
         /* show mask entry and move to next */
-         report_bitmasks(pt -> numOfLevels, pt ->bitMaskArr);
-    } else if(rSum) {
-        // ./pagetable -n 100000 -o summary trace.sample.tr 20
-        unsigned int page_size = sizeof(pt); // ?
-        unsigned int bytes = 0; // ?
+        report_bitmasks(pt->numOfLevels, pt->bitMaskArr);
+    }
+    // if -n summary is passed in on command line
+    else if (rSum)
+    {
+        //display size in bytes of pagetable, allocation of arrays and structs
+        //display hits, misses, percentage, number of addresses processed, and frames allocated
+        unsigned int page_size = sizeof(pt);
+        unsigned int bytes = sizeof(pt) + sizeof(lv) + sizeof(addys);
         printf("Page size: %d bytes\n", page_size);
         printf("Addresses processed: %d\n", addyCount);
-        double hit_percent = (double) pt->hits / addyCount * 100.0;
-        printf("Hits: %d (%.2f%%), Misses %d (%.2f%%)\n", 
-	    pt->hits, hit_percent, pt->misses, 100 - hit_percent);
+        double hit_percent = (double)pt->hits / addyCount * 100.0;
+        printf("Hits: %d (%.2f%%), Misses %d (%.2f%%)\n",
+               pt->hits, hit_percent, pt->misses, 100 - hit_percent);
         printf("Frames allocated: %d\n", pt->frameCount);
         printf("Bytes used:  %d\n", bytes);
     }
-  
-
-//CALCULATE ALL ACCURACIES AND OUTPUT
- fclose(fp);
+    //Finish reading addresses, close file
+    fclose(fp);
     return 0;
 }
 
 // ./pagetable -n 10000 trace.sample.tr 8 7 4
+// ./pagetable trace.sample.tr 8 12
--- a3/pagetable.cpp
+++ a3/pagetable.cpp
@@ -1,119 +1,159 @@
 #include "pagetable.h"
 using namespace std;
 
-int createPageTable(PAGETABLE *PageTable, char** num, int position)
+int createPageTable(PAGETABLE *PageTable, char **num, int index)
 {
-    PageTable->shifters = new unsigned int[PageTable -> numOfLevels];
+    //Initialize all of our struct PAGETABEL's arrays of # of shifted bits, the array of bitmasks per lvl and the amount of entires per lvl
+    PageTable->shifters = new unsigned int[PageTable->numOfLevels];
     PageTable->bitMaskArr = new unsigned int[PageTable->numOfLevels];
     PageTable->entryCount = new unsigned int[PageTable->numOfLevels];
 
-    int bitsUsed = 0;
+    int numOfBits = 0;
+    //Iterate through the amount of levels and add bit masks, shifted bits aand entry counts to respective arrays
     for (int i = 0; i < PageTable->numOfLevels; i++)
     {
-        int levelBits = atoi(num[position]);
-        PageTable->shifters[i] = 32 - bitsUsed - levelBits;
-        int maskStart = 32 - bitsUsed; //calculate bitmask for current level
-        PageTable->bitMaskArr[i] = calcBitmask(maskStart, levelBits);
-        PageTable->entryCount[i] = (1 << levelBits); //2 ^ levelBits
-        bitsUsed += levelBits;
-        position++;
-        if (bitsUsed >= 32)
+        int levelBits = atoi(num[index]);
+        PageTable->shifters[i] = 32 - numOfBits - levelBits;
+        int mask = 32 - numOfBits;
+        PageTable->bitMaskArr[i] = createBitMask(mask, levelBits);
+        PageTable->entryCount[i] = 1 << levelBits;
+        numOfBits += levelBits;
+        index++;
+        if (numOfBits >= 32)
         {
-            fprintf(stderr, "Logical address length is too long. Max: 32 bits\n");
+            //If your number of bits exceeds 32, we have to give an error and exit the program with error code 1
+            cout << "This address is over 32 bits" << endl;
             exit(1);
         }
     }
 
+    //Initialize our hits and misses for the page table to be allowed to calculate them for our summary
     PageTable->hits = 0;
     PageTable->misses = 0;
-    PageTable->rootPtr = initializeLevel(PageTable, PageTable->rootPtr, 0);
-    return bitsUsed;
+    PageTable->rootPtr = createLevel(PageTable, PageTable->rootPtr, 0);
+    //We also initialize our root pointer here to the first level of our page table
+    //Return the amount of bits that we used
+    return numOfBits;
 }
 
+//Logical to page just ANDs our bitmask at the respective level and the logical address to get our page # and then we shift it the correct amount of bits for that level
 unsigned int LogicalToPage(unsigned int LogicalAddress, unsigned int Mask, unsigned int Shift)
 {
-    return (LogicalAddress & Mask) >> Shift; // Return the logical address ANDed with the bitmask and shifted correct amount of bits to the right.
+    return (LogicalAddress & Mask) >> Shift;
 }
 
-unsigned int calcBitmask(int start, int length)
+//Function to create our bitmask for the given level which takes in our offset + that level index (1-n)
+unsigned int createBitMask(int pos, int length)
 {
-    unsigned int mask = (1 << length) - 1; // (2^length) - 1
-    mask <<= (start - length);             //shifts the 1's to the correct starting position
+    //Basically calculating 2^(length) - 1
+    unsigned int mask = (1 << length) - 1;
+    //Shift mask left and set equal to the position - length
+    mask <<= (pos - length);
+    //return mask for that level index
     return mask;
 }
 
-void PageInsertHELPER(LEVEL *LevelPtr, unsigned int LogicalAddress, unsigned int Frame)
+//Page insertion function for the PAGETABLE struct
+//Accepts level pointer, logical address, and the frame index
+void PageInsert(LEVEL *LevelPtr, unsigned int LogicalAddress, unsigned int Frame)
 {
+    //Basically assigning a pagetable pointer to a caste PAGETABLE pointer of a level pointer that points back to our pagetable pointer
+    //We're basically connecting our page table pointer to the level pointer that points to the pagetable again in order to allow for access from either end
     PAGETABLE *PageTable = (PAGETABLE *)LevelPtr->pageTablePtr;
-    unsigned int pageNumber = LogicalToPage(LogicalAddress, PageTable->bitMaskArr[LevelPtr -> depth], PageTable->shifters[LevelPtr -> depth]);
+    //Generate page # based on calling the logical to page function with the logical address, bitmask for that specific level index, and the shift # for that specific level index
+    unsigned int pageNumber = LogicalToPage(LogicalAddress, PageTable->bitMaskArr[LevelPtr->depth], PageTable->shifters[LevelPtr->depth]);
+    //Create a leaf node for our tree-structure PT that is our number of levels input - 1 to allow for enumeration
     unsigned int leafNode = (PageTable->numOfLevels) - 1;
-    
-    if (LevelPtr -> isLeafNode)
+    //We are checking if this level is a leaf node
+    if (LevelPtr->isLeafNode)
     {
+        //If it is, we go ahead and set its bit on the map as valid to show that there is an address here
+        //Also set the framae index = to whatever was passed through to page insert
+        //Lastly increment frameCount as to keep track of how many page frames were created
+        LevelPtr->map[pageNumber].flagIndex = true;
+        LevelPtr->map[pageNumber].frameIndex = Frame;
+        LevelPtr->pageTablePtr->frameCount++;
+    }
+    else
+    { //If the level is not a leaf node, we enter this
+        if (LevelPtr->nextLevelPtr[pageNumber] == NULL)
+        {
+            //Check fi our nextLevelPtr at this page # is NULL and if it is we create another level
+            LevelPtr->nextLevelPtr[pageNumber] = createLevel(LevelPtr->pageTablePtr, LevelPtr, LevelPtr->depth + 1);
+        }
+        //Insert the page no matter what
+        PageInsert(LevelPtr->nextLevelPtr[pageNumber], LogicalAddress, Frame);
+    }
+}
 
-        LevelPtr -> map[pageNumber].flagIndex = true;
-        LevelPtr -> map[pageNumber].frameIndex = Frame;
-        LevelPtr -> pageTablePtr -> frameCount++;
+//This is the function that takes in our page table pointer and we basically just use it to cast the same PT pointer to a level pointer and call our helper method along with pointing our casted level pointer to the rootPtr of the PT struct
+void PageInsert(PAGETABLE *PageTable, unsigned int LogicalAddress, unsigned int Frame)
+{
+    PageInsert((LEVEL *)(PageTable->rootPtr), LogicalAddress, Frame);
+}
+
+//Page Lookup will cast the given PT pointer to a level pointer and pass it the rootptr alongside the logical address to the helper function below
+MAP *PageLookup(PAGETABLE *PageTable, unsigned int LogicalAddress)
+{
+    return PageLookup((LEVEL *)PageTable->rootPtr, LogicalAddress);
+}
+//Looking four our page in the pagetable
+MAP *PageLookup(LEVEL *level, unsigned int LogicalAddress)
+{
+    unsigned int index = LogicalToPage(LogicalAddress, level->pageTablePtr->bitMaskArr[level->depth], level->pageTablePtr->shifters[level->depth]);
+    //We got our index or page # by calling logical to page
+    //Conditional check if it is a leaf node or no
+    if (level->isLeafNode)
+    {
+        //We can just use the index to see if it's set and if it is we can just return that page at that index
+        if (level->map[index].flagIndex)
+            return &level->map[index];
+        else
+        {
+            return NULL;
+        }
+        //if the map doesn't have a valid bit set for that page # it means that it's not inserted and we cannot return anything besides null
     }
     else
     {
-        // Create an array of Level * entries based upon the number of entries in the new level and initialize to null/invalid as appropriate
-        if (LevelPtr -> nextLevelPtr[pageNumber] == NULL){
-            LevelPtr->nextLevelPtr[pageNumber] = initializeLevel(LevelPtr->pageTablePtr, LevelPtr, LevelPtr->depth+1);
-        }
-        // PageInsert(pointer to new Level, Address, Frame)
-        PageInsertHELPER(LevelPtr->nextLevelPtr[pageNumber], LogicalAddress, Frame);
-    }
-}
-
-void PageInsert(PAGETABLE *PageTable, unsigned int LogicalAddress, unsigned int Frame)
-{
-    PageInsertHELPER((LEVEL *)(PageTable->rootPtr), LogicalAddress, Frame);
-}
-
-MAP *PageLookup(PAGETABLE *PageTable, unsigned int LogicalAddress) {
-    return PageLookupHELPER((LEVEL *)PageTable->rootPtr, LogicalAddress);
-}
-
-/* searches for a given logical address and returns a pointer to the map struct associated with the address, or NULL if address not found */
-MAP * PageLookupHELPER(LEVEL *level, unsigned int LogicalAddress) {
-    unsigned int index = LogicalToPage(LogicalAddress, level->pageTablePtr->bitMaskArr[level->depth], level->pageTablePtr->shifters[level->depth]);
-    // cout << index << endl;
-    // cout << level -> isLeafNode << endl;
-    if (level->isLeafNode) {        //test if current level is a leaf node
-        if (level->map[index].flagIndex)        //test if map[index] is valid
-            return &level->map[index];
-        else                        //page has been seen for the first time
+        //if it's not a leaf node we can check to see if it's in the next level and if there's no next level we will just return the page lookup of that page
+        if (level->nextLevelPtr[index] == NULL)
             return NULL;
-    }
-    else {
-        if (level->nextLevelPtr[index] == NULL)      //interior level at [index] not found, return NULL
-            return NULL;
-        return PageLookupHELPER((LEVEL*) level->nextLevelPtr[index], LogicalAddress);   //traverse to next level
+        return PageLookup((LEVEL *)level->nextLevelPtr[index], LogicalAddress);
     }
 }
 
-
-
-LEVEL *initializeLevel(PAGETABLE *PageTable, LEVEL *level, int depth)
+//Instantiation of our level struct for our pagetable
+LEVEL *createLevel(PAGETABLE *PageTable, LEVEL *level, int levelDepth)
 {
-    level = (LEVEL*)calloc(1, sizeof(LEVEL));
-    level->depth = depth;
-    level -> pageTablePtr = PageTable;
-    level->isLeafNode = (depth + 1 >= PageTable->numOfLevels);
+    //Allocate memory and fill with 0's for the given size of that level
+    level = (LEVEL *)calloc(1, sizeof(LEVEL));
+    //Set out depth and pagetableptr attributes for the struct to given variables
+    level->depth = levelDepth;
+    level->pageTablePtr = PageTable;
+    //check that if we are on the last level
+    if (PageTable->numOfLevels <= levelDepth + 1)
+    {
+        //if we are, we can set this level to be true that it is a leaf node
+        level->isLeafNode = true;
+    }
+    //If this level is a leafNode enter
     if (level->isLeafNode)
-    { //allocate maps for leaf nodes
-        level->map = (MAP *)calloc(PageTable->entryCount[depth], sizeof(MAP));
-        for (int i = 0; i < PageTable->entryCount[depth]; i++)
+    {
+        //Set our map pointer = a newly created map pointer allocated of size MAP with the correct entry count at this level using calloc to fill with 0s
+        level->map = (MAP *)calloc(PageTable->entryCount[levelDepth], sizeof(MAP));
+        //For the amount fo entries in this level at this depth, we wanat to go ahead and insert all empty bits into the map to show that it's empty right now
+        for (int i = 0; i < PageTable->entryCount[levelDepth]; i++)
         {
             level->map[i].flagIndex = false;
         }
     }
+    //If this is not a leaf node enter
     else
-    { //allocate next level pointers
-        level->nextLevelPtr = (LEVEL **)calloc(PageTable->entryCount[depth], sizeof(LEVEL *));
+    {
+        //We set our next level ptr for this level to be a newly instantiaated double pointer with allocated size of the level pointer and the correct amount of entries for this level at thsi depth from our page table.
+        level->nextLevelPtr = (LEVEL **)calloc(PageTable->entryCount[levelDepth], sizeof(LEVEL *));
     }
+    //return this level
     return level;
 }
-
-
